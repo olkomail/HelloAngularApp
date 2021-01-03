67c58932abb0fd0db1f12c9d944028618a18d6a0
olkomail <olkomail@gmail.com>
2021-01-03 10:21:53 +0200

    My first commit

---
+++ HelloAngularApp/Controllers/ProductController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using HelloAngularApp.Models;
+
+namespace HelloAngularApp.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductController : Controller
+    {
+        ApplicationContext db;
+        public ProductController(ApplicationContext context)
+        {
+            db = context;
+            if (!db.Products.Any())
+            {
+                db.Products.Add(new Product { Name = "iPhone X", Company = "Apple", Price = 79900 });
+                db.Products.Add(new Product { Name = "Galaxy S8", Company = "Samsung", Price = 49900 });
+                db.Products.Add(new Product { Name = "Pixel 2", Company = "Google", Price = 52900 });
+                db.SaveChanges();
+            }
+        }
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return db.Products.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Product Get(int id)
+        {
+            Product product = db.Products.FirstOrDefault(x => x.Id == id);
+            return product;
+        }
+
+        [HttpPost]
+        public IActionResult Post(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return Ok(product);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpPut]
+        public IActionResult Put(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Update(product);
+                db.SaveChanges();
+                return Ok(product);
+            }
+            return BadRequest(ModelState);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            Product product = db.Products.FirstOrDefault(x => x.Id == id);
+            if (product != null)
+            {
+                db.Products.Remove(product);
+                db.SaveChanges();
+            }
+            return Ok(product);
+        }
+    }
+}
---
+++ HelloAngularApp/Models/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HelloAngularApp.Models
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
